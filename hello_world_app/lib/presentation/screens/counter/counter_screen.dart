32b393e601281fdac5c939274855a30f533b9ee5
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-20 18:05:13 -0400

    Added button and text design

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -5,8 +5,23 @@ class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Counter Screen!!')),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              '10',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            Text('Clicks', style: TextStyle(fontSize: 25))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.plus_one),
+      ),
     );
   }
 }
