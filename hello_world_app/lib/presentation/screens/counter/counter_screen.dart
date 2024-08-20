5ffb9ba7fa01d194a93ad51cd161d635a017faa0
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-20 17:51:05 -0400

    Added new screen with its archive directory

---
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Counter Screen!!')),
+    );
+  }
+}
