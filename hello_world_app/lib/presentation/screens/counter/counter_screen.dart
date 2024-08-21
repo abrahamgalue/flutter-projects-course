d71e20c4ddfd15ee301113d02730b7dcf5d1bd82
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-21 14:53:34 -0400

    Added functionality to update the state

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter Screen'),
+        title: const Text('Counter Screen'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             Text(
-              '10',
-              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              '$clickCounter',
+              style:
+                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
-            Text('Clicks', style: TextStyle(fontSize: 25))
+            Text('Click${clickCounter == 1 ? '' : 's'}',
+                style: const TextStyle(fontSize: 25))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.plus_one),
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
+        child: const Icon(Icons.plus_one),
       ),
     );
   }
