233cdb994cb718d886ee81478b08962f5a762de0
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-22 17:29:41 -0400

    Added class to change the global style of the application

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 0).theme(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
